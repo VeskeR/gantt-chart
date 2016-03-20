4a3a71e93fe9a90aeedda3b64e0570a83d7dc4c9
Andrey Bulat <andrii.bulat@gmail.com>
2016-03-20 14:22:09 +0200

    Added chart template

---
+++ app/scripts/chart-template.js
@@ -0,0 +1,99 @@
+'use strict';
+var chart = {
+  startTime: 'Mar 1 2016',
+  endTime: 'Mar 31 2016',
+  rows: [
+    {
+      name: 'Requirements',
+      blocks: [
+        {
+          name: 'Review',
+          startTime: 'Mar 1 2016',
+          endTime: 'Mar 2 2016'
+        }
+      ]
+    },
+    {
+      name: 'Setup',
+      blocks: [
+        {
+          name: 'Setup',
+          startTime: 'Mar 2 2016',
+          endTime: 'Mar 3 2016'
+        }
+      ]
+    },
+    {
+      name: 'Development',
+      blocks: [
+        {
+          name: 'Coding',
+          startTime: 'Mar 2 2016',
+          endTime: 'Mar 4 2016'
+        }
+      ]
+    },
+    {
+      name: 'Testing',
+      blocks: [
+        {
+          name: 'Integration Tests',
+          startTime: 'Mar 4 2016',
+          endTime: 'Mar 5 2016'
+        },
+        {
+          name: 'Customer Tests',
+          startTime: 'Mar 5 2016',
+          endTime: 'Mar 6 2016'
+        }
+      ]
+    },
+    {
+      name: 'Deployment',
+      blocks: [
+        {
+          name: 'Setup',
+          startTime: 'Mar 6 2016',
+          endTime: 'Mar 7 2016'
+        },
+        {
+          name: 'Go-Live!',
+          startTime: 'Mar 7 2016',
+          endTime: 'Mar 8 2016'
+        }
+      ]
+    },
+    {
+      name: 'Training',
+      blocks: [
+        {
+          name: 'Training',
+          startTime: 'Mar 8 2016',
+          endTime: 'Mar 9 2016'
+        }
+      ]
+    },
+    {
+      name: 'Support',
+      blocks: [
+        {
+          name: 'Support',
+          startTime: 'Mar 9 2016',
+          endTime: 'Mar 20 2016'
+        }
+      ]
+    },
+    {
+      name: 'Project Management',
+      blocks: [
+        {
+          name: 'Project Management',
+          startTime: 'Mar 1 2016',
+          endTime: 'Mar 20 2016'
+        }
+      ]
+    }
+  ]
+}
+
+module.exports = chart;
