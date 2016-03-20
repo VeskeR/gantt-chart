2f1fa3edb36569abb4bb112c7228e1bba16c46c5
Andrey Bulat <andrii.bulat@gmail.com>
2016-03-20 19:02:30 +0200

    Added chart configuration on resize

--- app/scripts/chart-generator.js
+++ app/scripts/chart-generator.js
@@ -12,14 +12,17 @@ var ChartGenerator = function (chartTarget, chartJson) {
 
 ChartGenerator.prototype = {
   renderChart: function (chartTarget, chartJson) {
+    var self = this;
+    
     this._chartTarget = chartTarget || this._chartTarget;
     this._chartJson = chartJson || this._chartJson;
 
     this._createChartElement();
     this._appendChartToTarget();
-    console.log('Chart DOM element:');
-    console.log(this._chartElement);
     this._configureChart();
+    window.addEventListener('resize', function () {
+      self._configureChart();
+    });
   },
   _createChartElement: function () {
     this._chartElement = generateChartHtml(this._chartJson);
